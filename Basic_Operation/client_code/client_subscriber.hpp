12c8780c97aed7624609df01e3d430233d77a5d1
EricZhang12138 <2067716083zyo@gmail.com>
2025-12-21 23:40:45 +0000

    In the client_subscriber.hpp I made sure that all the different messages are handled the same way. Basically when a notification message is sent, it invalidates the current cache, and then we only need to erase its corresponding entry in all the caches on the client so next time when we want to access the file, we do that through the network to the server and get the latest data.

--- Basic_Operation/client_code/client_subscriber.hpp
+++ Basic_Operation/client_code/client_subscriber.hpp
@@ -23,38 +23,35 @@ void FileSystemClient::RunSubscriber() {
         std::string path_on_server = note.directory() + (note.directory().back() == '/' ? "" : "/");
         std::string path_on_client = std::string("./tmp/cache") + (path_on_server.front()=='/'? "" : "/") + path_on_server;
 
-        if (note.message() == "UPDATE") {
             // we will erase the corresponding file in cache if we want to update
             // it is important to note that two clients can't open the same file at the same time
             // otherwise, if one flushes its changes to the server and the other still has the file opened and the file in cache is removed
             // It can lead to undefined behaviour
 
             // check if file is opened
-            cache_mutex.lock();
-            if (opened_files.find(path_on_client) != opened_files.end()){
-                std::cout << "Error: File currently open and updates from server failed for " << path_on_client << std::endl;
-                cache_mutex.unlock();
-                continue; // go to the top of the while loop and abort this update
-            }
-            auto it = cache.find(path_on_client);
-            if (it != cache.end()){ // we can find the file in cache. Since we registered, it should always be in cache
-                cache.erase(it); // erase the cache
-            }else{ // not in cache
-                std::cout << "Inconsistent State: File: " << path_on_client << " is registered but not in cache"<< std::endl;
-                cache_mutex.unlock();
-                continue; 
-            }
-            auto its = cached_attr.find(path_on_server);
-            if (its != cached_attr.end()){
-                cached_attr.erase(its);
-            }else{
-                std::cout << "Inconsistent State: File: " << path_on_server << " is registered but not in cache"<< std::endl;
-                cache_mutex.unlock();
-                continue; 
-            }
+        cache_mutex.lock();
+        if (opened_files.find(path_on_client) != opened_files.end()){
+            std::cout << "Error: File currently open and updates from server failed for " << path_on_client << std::endl;
             cache_mutex.unlock();
+            continue; // go to the top of the while loop and abort this update
         }
-        // Handle DELETE, RENAME, etc.
+        auto it = cache.find(path_on_client);
+        if (it != cache.end()){ // we can find the file in cache. Since we registered, it should always be in cache
+            cache.erase(it); // erase the cache
+        }else{ // not in cache
+            std::cout << "Inconsistent State: File: " << path_on_client << " is registered but not in cache"<< std::endl;
+            cache_mutex.unlock();
+            continue; 
+        }
+        auto its = cached_attr.find(path_on_server);
+        if (its != cached_attr.end()){
+            cached_attr.erase(its);
+        }else{
+            std::cout << "Inconsistent State: File: " << path_on_server << " is registered but not in cache"<< std::endl;
+            cache_mutex.unlock();
+            continue; 
+        }
+        cache_mutex.unlock();
     }
 
     grpc::Status status = reader->Finish();
